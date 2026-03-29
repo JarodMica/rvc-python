782467ababe17698a4b5100aedfe16e69cebaa56
Jarod Mica <jarodmica@gmail.com>
2026-03-29 13:19:19 -0700

    Update library to change where sources are downloaded from

--- rvc_python/download_model.py
+++ rvc_python/download_model.py
@@ -13,9 +13,8 @@ def download_rvc_models(this_dir):
         os.makedirs(folder)
     
     files = {
-        "hubert_base.pt": "https://huggingface.co/daswer123/rvc_base/resolve/bbb6736b97a98df0a87fe3592c0a061c53f0a75f/hubert_base.pt?download=true",
-        "rmvpe.pt": "https://huggingface.co/daswer123/rvc_base/resolve/bbb6736b97a98df0a87fe3592c0a061c53f0a75f/rmvpe.pt?download=true",
-        "rmvpe.onnx": "https://huggingface.co/daswer123/rvc_base/resolve/bbb6736b97a98df0a87fe3592c0a061c53f0a75f/rmvpe.onnx?download=true"
+        "hubert_base.pt": "https://huggingface.co/Jmica/audiobook_models/resolve/main/hubert_base.pt?download=true",
+        "rmvpe.pt": "https://huggingface.co/Jmica/audiobook_models/resolve/main/rmvpe.pt?download=true"
     }
     
     for filename, url in files.items():
